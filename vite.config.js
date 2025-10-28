67910d7d22cf8b7841d554af807b9efa6b26898e
hismyhill <owolabismaeel@gmail.com>
2025-10-29 00:17:38 +0100

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -22,4 +22,9 @@ export default defineConfig({
       exclude: ["node_modules/", "src/setupTests.js"],
     },
   },
+  build: {
+    rollupOptions: {
+      external: ["lucide-react"],
+    },
+  },
 });
